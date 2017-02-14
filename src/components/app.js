f7f3d83db4c74ed08c214482ee4fdc6fbb0c8605
Ian Leeds <ileeds@brandeis.edu>
2017-02-14 16:05:52 +0000

    no ipad

--- src/components/app.js
+++ src/components/app.js
@@ -5,25 +5,6 @@ import { h, Component } from 'preact';
 import Iphone from './iphone';
 
 export default class App extends Component {
-//var App = React.createClass({
-
-	// once the components are loaded, checks if the url bar has a path with "ipad" in it, if so sets state of tablet to be true
-	componentDidMount() {
-		const urlBar = window.location.href;
-		if(urlBar.includes("ipad")) {
-			this.setState({
-				"isTablet": true
-			});
-		} else {
-			this.setState({
-				"isTablet": false
-			});
-		}
-	}
-
-	/*
-		A render method to display the required Component on screen (iPhone or iPad) : selected by checking component's isTablet state
-	*/
 	render(){
 		return (
 			<div id="app">
