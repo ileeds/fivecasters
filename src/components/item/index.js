c0d4c5336f1ecce89ceeedc8b81a3f4a36ca44ba
vivekwarriar <vivekwarriar@hotmail.co.uk>
2017-02-28 13:25:55 +0000

    Some style to the suggestions area: INCOMPLETE

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,16 +1,32 @@
 // import preact
-import { h, render, Component } from 'preact';
+import {h, render, Component} from 'preact';
 import style from './style_iphone';
+import itemIcon from '../../assets/icons/itemIconWhite.png';
 
 export default class Item extends Component {
 
-  render() {
-    return (
+	render() {
+		return (
 			<div id='item' class={ style.item }>
-        <img src={this.props.place.photo} alt="some_text" style="width:100%;height:150px;"></img>
-        {JSON.stringify(this.props.place)}
+				<div className={style.imgContainer}>
+					<img src={this.props.place.photo} alt="some_text" ></img>
+				</div>
+				<div className="blueBox">
+					<ul>
+						<li><img src={itemIcon} alt="weather of area"/></li>
+						<li>10 clear</li>
+						<li>{this.props.place.hours}</li>
+					</ul>
+				</div>
+				<div className="titleDiv">
+					<h1>{this.props.place.name}</h1>
+					<p>{this.props.place.keyword}</p>
+					<span>{this.props.place.price}</span>
+				</div>
+				<p className="address">{this.props.place.address}</p>
+
 			</div>
 		);
-  }
+	}
 
 }
