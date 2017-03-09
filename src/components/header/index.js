41cca5a8861db7a31e3af747504df52b8a1a4bb1
vivekwarriar <vivekwarriar@hotmail.co.uk>
2017-03-09 16:12:05 +0000

    Minor design changes

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,7 +8,7 @@ export default class Header extends Component {
 	render() {
     return (
 			<div style="z-index: 5; background: white; position:fixed; height:50px; width:100%" onClick={() => window.scrollTo(0, 0)}>
-				<h3 class={style.settings}>SunDiner</h3>
+				<p class={style.settings}>Sun<b>Diner</b></p>
 			</div>
 		);
   }
