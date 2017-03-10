810320f795a1d48db96a21c18090c929309978a0
Ian Leeds <ileeds@brandeis.edu>
2017-03-10 12:16:59 +0000

    clean

--- src/components/weather/index.js
+++ src/components/weather/index.js
@@ -5,21 +5,21 @@ import Suggest from '../suggest/index';
 import Header from '../header/index';
 import Icon from '../icon/index';
 import Slider from '../slider/index';
-import helper from '../../helpers'
+import helper from '../../helpers';
 
 
 export default class Weather extends Component {
 
 	getInitialState() {
-    return {
-      rain: "none"
-    };
-  }
+		return {
+			rain: "none"
+		};
+	}
 
 	render() {
 		//load until state retrieved
 		if (this.state.rain === undefined) return <div />;
-    return (
+		return (
 			<div class={style.container}>
 				<Header />
 				<div style ="margin-top: 75px;">
@@ -38,16 +38,16 @@ export default class Weather extends Component {
 				<Suggest temp = {this.state.temp} con = {this.state.con} rain = {this.state.rain} loc = {this.state.loc} time = {this.state.time}/>
 			</div>
 		);
-  }
+	}
 
 	// a call to fetch weather data via wunderground
 	componentDidMount() {
-		var now = null;
-		var hours = {};
-		var self = this;
+		let now = null;
+		let hours = {};
+		const self = this;
 		navigator.geolocation.getCurrentPosition(function(location) {
-			var wunderground = require('wunderground')(""+process.env.WUNDERGROUND);
-			var query = {
+			const wunderground = require('wunderground')(""+process.env.WUNDERGROUND);
+			const query = {
 		    lat  : ''+location.coords.latitude,
 		    lng : ''+location.coords.longitude
 			};
@@ -73,19 +73,20 @@ export default class Weather extends Component {
 				throw err;
 			} else {
 				now = helper.conditions(data);
-				var d = new Date();
-				self.setState({rain:now.prec,
-											temp:now.temp,
-											con:now.con,
-											loc:location,
-											time:d.getHours()}
-				);
+				const d = new Date();
+				self.setState({
+					rain: now.prec,
+					temp: now.temp,
+					con: now.con,
+					loc: location,
+					time: d.getHours()
+				});
 				//render page after getting state
 				self.forceUpdate();
 				//conditionally render weather image based on conditions
 				helper.picReplace(now.con, d.getHours(), location);
 
-				var nowDoc = document.getElementById('now');
+				const nowDoc = document.getElementById('now');
 				nowDoc.value = now.prec;
 				nowDoc.className += ' nowWeather';
 				nowDoc.innerHTML = "<h1>"+now.loc+"</h1><br/><h2>"+now.temp+"\xB0C</h2><br/><h3>"+now.con+"</h3>";
@@ -95,29 +96,29 @@ export default class Weather extends Component {
 				var hourDoc = document.getElementById('wrap');
 				hourDoc.appendChild(hours[0]);
 				//form array of times from now through 24 hours
-				var n = d.getHours()+1;
-				var j = n+23;
-				var times = [];
-				for (var i=n; i<j; i++){
+				const n = d.getHours()+1;
+				const j = n+23;
+				let times = [];
+				for (let i=n; i<j; i++){
 					if (i>23){
 						i=0;
 						j=n-1;
 					}
 					times.push(i);
 				}
-				var slide = document.getElementById('wun');
+				const slide = document.getElementById('wun');
 				document.getElementById('start').onclick = function(e) {
-					var d = new Date();
-					var n = d.getHours();
+					const d = new Date();
+					const n = d.getHours();
 					helper.timeClick(e, self, hours, nowDoc);
-				}
+				};
 				for (var hour in times) {
-					var single = document.createElement('div');
+					const single = document.createElement('div');
 					single.className = style.hour;
 					single.innerHTML = times[hour]+":00";
 					single.onclick = function(e) {
 						helper.timeClick(e, self, hours, nowDoc);
-					}
+					};
 					slide.appendChild(single);
 				}
 				self.alerts(wunderground, query);
@@ -129,13 +130,11 @@ export default class Weather extends Component {
 		wunderground.alerts(query, function(err, data) {
 			if (err) {
 				throw err;
-			} else {
-				if (data['alerts'].length > 0) {
-					var d = new Date();
-					var dd = new Date(data['alerts'][0]['expires']);
-					if (dd>d){
-						window.alert(data['alerts'][0]['description']);
-					}
+			} else if (data['alerts'].length > 0) {
+				const d = new Date();
+				const dd = new Date(data['alerts'][0]['expires']);
+				if (dd>d){
+					window.alert(data['alerts'][0]['description']);
 				}
 			}
 		});
